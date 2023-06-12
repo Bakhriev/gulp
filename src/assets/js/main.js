f7701ffd79d6e5103800ef0e3b32fb0edfcbe51e
Bakhriev <legendofpain5112@gmail.com>
2023-06-12 21:35:10 +0500

    add features

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,27 @@
-import './functions/burger.min.js'
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const navigation = document.querySelector('.header__navigation')
+  const overlay = document.querySelector('.overlay')
 
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    navigation.classList.toggle('active')
+    overlay.classList.toggle('active')
+  })
 
+  overlay.addEventListener('click', () => {
+    burger.classList.remove('active')
+    navigation.classList.remove('active')
+    overlay.classList.remove('active')
+  })
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      burger.classList.remove('active')
+      navigation.classList.remove('active')
+      overlay.classList.remove('active')
+    }
+  })
+}
+
+burgerMenu()
