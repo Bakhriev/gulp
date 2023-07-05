aae17748501bddc15e46ccfea26d29343339e814
Bakhriev <legendofpain5112@gmail.com>
2023-07-06 03:30:57 +0500

    .

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,25 +1,21 @@
-function burgerMenu() {
+
+const burgerMenu = () => {
   const burger = document.querySelector('.burger')
   const navigation = document.querySelector('.header__navigation')
   const overlay = document.querySelector('.overlay')
+  const elements = [burger, navigation, overlay]
 
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
-    navigation.classList.toggle('active')
-    overlay.classList.toggle('active')
-  })
+  const toggleActiveClass = () =>
+    elements.forEach(element => element.classList.toggle('active'))
 
-  overlay.addEventListener('click', () => {
-    burger.classList.remove('active')
-    navigation.classList.remove('active')
-    overlay.classList.remove('active')
-  })
+  burger.addEventListener('click', toggleActiveClass)
+
+  overlay.addEventListener('click', toggleActiveClass)
 
   window.addEventListener('resize', () => {
-    if (window.innerWidth > 991.98) {
-      burger.classList.remove('active')
-      navigation.classList.remove('active')
-      overlay.classList.remove('active')
+    const { innerWidth } = window
+    if (innerWidth > 991.98) {
+      elements.forEach(element => element.classList.remove('active'))
     }
   })
 }
@@ -29,7 +25,7 @@ burgerMenu()
 const dropdownInit = () => {
   const allDropdowns = document.querySelectorAll('[data-dropdown]')
 
-  if (window.innerWidth > 992) {
+  if (window.innerWidth > 992.98) {
     document.addEventListener('click', e => {
       let currentDropdown
       if (e.target.closest('[data-dropdown]')) {
@@ -68,3 +64,106 @@ const dropdownInit = () => {
   }
 }
 dropdownInit()
+
+export const magnifierFunc = () => {
+  const imgContainer = document.querySelector('.img-container')
+  const img = document.querySelector('.img-container img')
+  const magnifier = document.querySelector('.magnifier')
+
+  let flag = false
+
+  imgContainer.addEventListener('mouseover', () => {
+    magnifier.style.display = 'block'
+    flag = true
+  })
+  imgContainer.addEventListener('mouseout', () => {
+    magnifier.style.display = 'none'
+    flag = false
+  })
+
+  imgContainer.addEventListener('mousemove', e => {
+    if (flag) {
+      const { clientX, clientY } = e
+
+      const x = clientX - imgContainer.offsetLeft
+      const y = clientY - imgContainer.offsetTop
+
+      magnifier.style.left = `${x}px`
+      magnifier.style.top = `${y}px`
+      console.log(magnifier.backgroundSize)
+      const bgX = (100 * x) / img.offsetWidth
+      const bgY = (100 * y) / img.offsetHeight
+
+      magnifier.style.backgroundPosition = `${bgX}% ${bgY}%`
+    }
+  })
+}
+
+
+export function rippleEffect(e) {
+  const x = e.clientX - e.target.offsetLeft
+  const y = e.clientY - e.target.offsetTop
+
+  const span = document.createElement('span')
+  span.style.left = `${x}px`
+  span.style.top = `${y}px`
+
+  console.log(this)
+  this.appendChild(span)
+  setTimeout(() => {
+    span.remove()
+  }, 800)
+}
+
+// const button = document.querySelector('.btn-r')
+// button.addEventListener('click', rippleEffect)
+
+const navLinks = document.querySelectorAll('.duplicate')
+
+if (window.innerWidth > 1024) {
+  navLinks.forEach(link => {
+    const spans1 = link.querySelectorAll('.v-text-1 span')
+    const spans2 = link.querySelectorAll('.v-text-2 span')
+
+    link.addEventListener('mouseover', () => {
+      BounceText(spans1)
+      BounceText(spans2, 100, 80, 10)
+    })
+
+    link.addEventListener('mouseleave', () => {
+      spans1.forEach(span => {
+        span.style.transform = `translate(0, 0)`
+      })
+      spans2.forEach(span => {
+        span.style.transform = `translate(0, 100%)`
+      })
+    })
+  })
+}
+
+function BounceText(
+  spans,
+  transformRate = 110,
+  durationIncrement = 40,
+  delayIncrement = 20
+) {
+  let delay = 0
+  let duration = 250
+  spans.forEach(span => {
+    span.style.transition = `transform ${duration}ms ease-in-out`
+    span.style.transitionDelay = `${delay}ms`
+    span.style.transform = `translate(0, -${transformRate}%)`
+
+    if (delay < 100) {
+      delay += delayIncrement
+    } else {
+      delay += delayIncrement / 2
+    }
+
+    if (duration < 300) {
+      duration += durationIncrement
+    } else {
+      duration += durationIncrement / 2
+    }
+  })
+}
