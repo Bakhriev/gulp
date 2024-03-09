dc7ebb52d52ff2c946819bada509877ff79a6577
Bakhriev <muradbakhriev@gmail.com>
2024-03-09 19:24:16 +0500

    почистил мусор

--- src/assets/js/functions/rippleEffect.js
+++ src/assets/js/functions/rippleEffect.js
@@ -5,19 +5,19 @@
 export const rippleEffect = selector => {
 	document.querySelectorAll(selector).forEach(elem => {
 		elem.addEventListener("click", e => {
-			const {offsetLeft, offsetTop} = elem
-			const {clientX, clientY} = e
+			const {offsetLeft, offsetTop} = elem;
+			const {clientX, clientY} = e;
 
-			const ripples = document.createElement("span")
-			ripples.className = "ripple-effect__circle"
-			ripples.style.left = `${clientX - offsetLeft}px`
-			ripples.style.top = `${clientY - offsetTop}px`
+			const ripples = document.createElement("span");
+			ripples.className = "ripple-effect__circle";
+			ripples.style.left = `${clientX - offsetLeft}px`;
+			ripples.style.top = `${clientY - offsetTop}px`;
 
-			elem.appendChild(ripples)
+			elem.appendChild(ripples);
 
 			setTimeout(() => {
-				ripples.remove()
-			}, 1000)
-		})
-	})
-}
+				ripples.remove();
+			}, 1000);
+		});
+	});
+};
